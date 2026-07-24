b036169826e5544436941570a486c1483bd9f381
Emil gisslen bezjak <plowemil@Emils-MacBook-Air.local>
2026-07-25 00:09:34 +0200

    sync native ios config with capgo social login setup

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -8,6 +8,13 @@ const config: CapacitorConfig = {
     url: 'https://www.scanify.world',
     cleartext: false,
   },
+  plugins: {
+    SocialLogin: {
+      google: {
+        iOSClientId: '331795866866-p13hvpk792sn2qihd0bcp94j0c9dhscn.apps.googleusercontent.com',
+      },
+    },
+  },
 };
 
 export default config;
